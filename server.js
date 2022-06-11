92a3dc70f1d588629677f05117c7db4d89a23f18
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-06-11 20:26:54 +0530

    Basic express setup with body parser done

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log('Server Started');
+})
