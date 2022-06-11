e73bcd615c37006e3bd42c6622bf2a13d1d0b463
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-06-11 20:29:34 +0530

    Configured dotenv in the project

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server Started');
 })
